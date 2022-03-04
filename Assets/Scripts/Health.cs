71057c06346751c9b922a5ef74f73a00b490ae11
Yigithan Guven <demirejderha@gmail.com>
2022-03-04 17:43:48 +0300

    Health system added

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+    public int numberOfHearts;
+    public Image[] hearts;
+    public Sprite fullHearth;
+    public Sprite emptyHearth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i<health)
+            {
+                hearts[i].sprite = fullHearth;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHearth;
+            }
+            if (i<numberOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,12 +9,15 @@ public class PlayerController : MonoBehaviour
     public float SwipeSpeed = 10f;
 
     private bool isGameStarted=false;
+    private bool isGameFinished=false;
 
     private Transform localTrans;
     private Vector3 lastMousePos;
     private Vector3 mousePos;
     private Vector3 newPosForTrans;
     private Animator animator;
+
+    private Health healthSytsem;
 
     [SerializeField]
     private float swerveSpeed = 0.5f;
@@ -32,6 +35,7 @@ public class PlayerController : MonoBehaviour
     {
         localTrans = GetComponent<Transform>();
         animator = GetComponent<Animator>();
+        healthSytsem = this.GetComponent<Health>();
     }
 
     // Update is called once per frame
@@ -42,45 +46,52 @@ public class PlayerController : MonoBehaviour
             Vector3 translate = (new Vector3(0, 0, 1) * Time.deltaTime) * Speed;
             transform.Translate(translate);
         }
+        if (!isGameFinished) //Lock Control After victory
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                isGameStarted = true;
+                animator.SetBool("IsMoving", true);
+                _lastFrameFingerPositionX = Input.mousePosition.x;
+                //Play Animation Here
 
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            animator.SetBool("IsWon", true);
-            animator.SetBool("IsMoving", false);
-        }
-        if (Input.GetMouseButtonDown(0))
-        {
-            isGameStarted = true;
-            animator.SetBool("IsMoving", true);
-            _lastFrameFingerPositionX = Input.mousePosition.x;
-            //Play Animation Here
+            }
+            else if (Input.GetMouseButton(0))
+            {
+                _moveFactorX = Input.mousePosition.x - _lastFrameFingerPositionX;
+                _lastFrameFingerPositionX = Input.mousePosition.x;
+                MovePlayer();
 
+            }
+            else if (Input.GetMouseButtonUp(0) && !isGameFinished)
+            {
+                _moveFactorX = 0f;
+                Vector3 translate = (new Vector3(0, 0, 1) * Time.deltaTime) * Speed;
+                transform.Translate(translate);
+                //animator.SetBool("IsMoving", false);
+            }
         }
-        else if (Input.GetMouseButton(0))
-        {
-            _moveFactorX = Input.mousePosition.x - _lastFrameFingerPositionX;
-            _lastFrameFingerPositionX = Input.mousePosition.x;
-            MovePlayer();
 
-        }  
-        else if (Input.GetMouseButtonUp(0))
+        //Loose Condition
+        if (healthSytsem.health==0)
         {
-            _moveFactorX = 0f;
-            Vector3 translate = (new Vector3(0, 0, 1) * Time.deltaTime) * Speed;
-            transform.Translate(translate);
-            //animator.SetBool("IsMoving", false);
+            Debug.Log("Game Over");
+            isGameFinished = true;
+            isGameStarted = false;
         }
+        
     }
 
     public void MovePlayer()
     {
-        float swerveAmount = Time.deltaTime * swerveSpeed * MoveFactorX;
-        swerveAmount = Mathf.Clamp(swerveAmount, -maxSwerveAmount, maxSwerveAmount);
-        transform.Translate(swerveAmount , 0, 0);
-        //Limit player movment in x axis
-        float xPos = Mathf.Clamp(transform.position.x, xPosMin, xPosMax);
-        transform.position = new Vector3(xPos, transform.position.y, transform.position.z);
-
+        
+            float swerveAmount = Time.deltaTime * swerveSpeed * MoveFactorX;
+            swerveAmount = Mathf.Clamp(swerveAmount, -maxSwerveAmount, maxSwerveAmount);
+            transform.Translate(swerveAmount, 0, 0);
+            //Limit player movment in x axis
+            float xPos = Mathf.Clamp(transform.position.x, xPosMin, xPosMax);
+            transform.position = new Vector3(xPos, transform.position.y, transform.position.z);
+        
     }
 
     private void OnTriggerEnter(Collider other)
@@ -91,10 +102,12 @@ public class PlayerController : MonoBehaviour
         }
         if (other.gameObject.CompareTag("barrier"))
         {
+            healthSytsem.health = healthSytsem.health-1;
             Destroy(other.gameObject);
         }
         if (other.gameObject.CompareTag("Goal"))
         {
+            isGameFinished=true;
             animator.SetBool("IsWon", true);
             animator.SetBool("IsMoving", false);
             isGameStarted = false;
